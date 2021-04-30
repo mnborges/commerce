a6f69d1f63fff3461f0e74d437f74c20a55a8c9a
Maieza B <maieza.borges@gmail.com>
2021-04-30 13:55:57 -0300

    created listing model

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Listing
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username","first_name","last_name", "is_staff")
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "start_date","title","seller","status")
+    
+admin.site.register(User, UserAdmin)
+admin.site.register(Listing, ListingAdmin)
--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"@{self.username}: {self.email} ({self.first_name} {self.last_name}) "
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    start_bid =  models.DecimalField(max_digits= 8, decimal_places=2)
+    start_date = models.DateTimeField(auto_now=True)
+    end_date = models.DateTimeField(blank=True)
+    status = models.BooleanField(default=True)
+    image = models.URLField(blank=True) 
+    category = models.CharField(unique=True, blank=True, max_length=64)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    def __str__(self):
+        st = 'open' if self.status==True else  'closed'
+        return f"listing of {self.title} created by {self.seller.username} is {st}"
