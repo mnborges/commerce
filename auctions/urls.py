f41574e88db7e439df3c06b6b6063eead9eec8cf
Maieza B <maieza.borges@gmail.com>
2021-05-05 17:05:28 -0300

    added watchlist and categories functions/pages

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("new", views.new_listing, name="new_listing"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("categories", views.categories, name="categories"),
+    path("categories/<str:category_name>", views.category, name="category"),
     path("<int:listing_id>/view", views.listing_page, name="listing_page")
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -3,15 +3,16 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
 from .models import *
 
 
 def index(request):
-    active_listing = list(Listing.objects.filter(status=True))
+    active_listing = Listing.objects.filter(status=True)
     return render(request, "auctions/index.html",{
-        "active_listing": active_listing
+        "listings": active_listing
     })
 
 def login_view(request):
@@ -66,22 +67,25 @@ def register(request):
 @login_required(login_url='../login')
 def new_listing(request):
     if request.method == "POST":
-        user_id = request.POST["user_id"]
         title = request.POST["title"]
         description = request.POST["description"]
         start_bid = float(request.POST["start_bid"])
         category = request.POST["category"]
         image = request.POST["image_url"]
-        seller = User.objects.get(pk=user_id)
+        seller = User.objects.get(pk=request.user.id)
+        if not Group.objects.filter(category=category): Group(category=category).save()  
+        category = Group.objects.get(category=category)
         try:
-            listing = Listing(title=title, description=description,start_bid=start_bid, image=image, category=category, seller=seller)
+            listing = Listing(title=title, description=description,start_bid=start_bid, image=image, group=category, seller=seller)
             listing.save()
         except:
             return render(request, "auctions/new_listing.html",{
                 "message": "Please fill all required field correctly"
             })
         return HttpResponseRedirect(reverse("index"))
-    return render(request, "auctions/new_listing.html")
+    return render(request, "auctions/new_listing.html",{
+        "groups": Group.objects.all()
+    })
     
 def listing_page(request, listing_id):
     message= ""
@@ -89,8 +93,7 @@ def listing_page(request, listing_id):
     current_bid = listing.bids.last()
     if request.method == "POST": #someone placed a bid, a comment, or wants to close the auction
         action = request.POST['action']
-        user_id = request.POST['user_id']
-        user = User.objects.get(pk=user_id)
+        user = User.objects.get(pk=request.user.id)
         if action == 'bid': 
             value = float(request.POST['bid_value'])
             if ((current_bid and value <= current_bid.value) or value < listing.start_bid): #checks if bid is valid
@@ -112,17 +115,66 @@ def listing_page(request, listing_id):
                 message="Your comment could not be posted."
         elif action == 'close':
             listing.status = False #status False closes the auction
+            listing.end_date = datetime.now()
             try:
                 listing.save() 
                 return HttpResponseRedirect(reverse("listing_page", args=(listing_id,)))
             except:
                 message="Could not close the auction."
         else:
-            return HttpResponseRedirect(reverse("index"))    
-    comments = listing.comments.all()
+            return HttpResponseRedirect(reverse("index"))
     return render(request, "auctions/listing_page.html",{
         "listing":listing,
-        "comments": comments,
+        "on_watch": listing.on_watch.all(),
+        "comments": listing.comments.all(),
         "current_bid": current_bid,
         "message": message
     })
+    
+@login_required(login_url='../login')
+def watchlist(request):
+    message=''
+    user = User.objects.get(pk=request.user.id)
+    if request.method == "POST":
+        listing = Listing.objects.get(pk=request.POST['listing_id'])
+        if request.POST['action'] == 'add':
+            try: 
+                listing.on_watch.add(user)
+                listing.save()
+            except: 
+                message= 'Could not add item to your watchlist.'
+        elif request.POST['action'] == 'remove':  
+            try:
+                listing.on_watch.remove(user)
+                listing.save()
+            except:
+                message = 'Could not remove item from your watchlist'
+        return render(request, "auctions/listing_page.html",{
+                "listing": listing,
+                "on_watch": listing.on_watch.all(),
+                "comments": listing.comments.all(),
+                "current_bid": listing.bids.last(),
+                "message": message
+            })
+    return render(request, "auctions/watchlist.html",{
+        "listings": user.watchlist.all()
+    })
+def categories(request):
+    return render(request, "auctions/categories.html",{
+        "groups": Group.objects.all()
+    })
+    
+def category(request, category_name):
+    flag=False
+    try:
+        category = Group.objects.get(category=category_name)
+        listings = category.listings.all()
+    except: 
+        listings=[]
+        flag = True
+    return render(request, "auctions/category.html",{
+        "listings": listings,
+        "category_name": category_name,
+        "flag": flag
+    })
+        
